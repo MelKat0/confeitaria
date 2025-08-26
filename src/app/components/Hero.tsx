836dd5c37e7f96cd98ddce2e3eff00b1a4e41baa
Melissa Kato <melsakdc@gmail.com>
2025-08-26 12:01:50 -0300

    componentes

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,23 @@
+import "./Hero.css";
+
+export default function Hero(){
+  return(
+    <section className="hero-section">
+      <div className="hero-text"> 
+        <h1 className="hero-title">BEM VINDO ao</h1>
+        <h2 className="hero-subtitle">Quitiliano's Cakes</h2>
+        <h3 className="hero-phrase">
+            Nada como um doce caseiro pra adoçar o dia!
+        </h3>
+        <p className="hero-description">
+            Nossos bolos de pote são feitos com ingredientes 
+            selecionados, muito carinho e aquele sabor irresistível que 
+            conquista a cada colherada.
+        </p>
+      </div>
+      <div className="hero-image">
+        <img src="bolo.png" alt="bolo torta" />
+      </div>
+    </section>
+  );
+}
---
+++ src/app/data/Produtos.ts
@@ -0,0 +1,26 @@
+export const boloDePote = [
+    {   
+        title:"Moranguinho",
+        description:"Massa branca, recheio de ninho com pedaços de morango!",
+        price:"R$10,00",
+        image:"bolo1.png",
+    },
+    {   
+        title:"Brigadeiro",
+        description:"Massa de chocolate, com recheio de cookies e brigadeiro",
+        price:"R$10,00",
+        image:"bolo2.png",
+    },
+    {   
+        title:"Paçoquinha",
+        description:"Massa mista, recheio de amendoim e paçoca",
+        price:"R$10,00",
+        image:"bolo3.png",
+    },
+    {   
+        title:"Ninho com Nutella",
+        description:"Massa mista, recheio de ninho com nutella",
+        price:"R$10,00",
+        image:"bolo4.png",
+    }
+]
